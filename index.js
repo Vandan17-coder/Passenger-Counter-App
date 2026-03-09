679448fc39e4bc266ca52ae9d80bf916001e5d77
Vandan17 <vandanp1705@gmail.com>
2026-03-10 01:26:53 +0530

    Save button,set count to 0 on save, log count to console

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-//   document.getElementById("count-el").innerText = 5
+//   document.getElementById("count-el").innerText = 0
 
+let saveEl = document.getElementById("save-el");
+let countEl = document.getElementById("count-el");
 let count = 0;
 
 function increment(){
-   console.log("clicked");
     count++;
-    console.log(count);
+    countEl.innerText = count;
+}
+
+
+
+function save(){
+    let countStr = count  + " - ";
+    saveEl.textContent += countStr;
+    countEl.innerText = 0;
+    count = 0;
 }
